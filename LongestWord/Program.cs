a17f0c942eb2909806283719796f38190353f2fd
brendantyler <galmanbrendan@gmail.com>
2023-01-16 13:44:04 -0600

    Update Longest Word

---
+++ LongestWord/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Please Provide a string you want to find the longest word of");
+string? findLong = Console.ReadLine();
+
+bool checkString = true;
+
+while (checkString)
+{
+    if (findLong == "" || findLong == null)
+    {
+        Console.WriteLine("Please Enter a valid string");
+        findLong = Console.ReadLine();
+    } else
+    {
+        checkString = false;
+    }
+}
+
+findLong = Regex.Replace(findLong, @"[^\w\d\s]", "");
+string[] toWords = findLong.Split(new[] { ' ' });
+
+
+int currentLongest = 0;
+string longestWord = string.Empty;
+
+foreach (string word in toWords)
+{
+    if (word.Length > currentLongest)
+    {
+        currentLongest= word.Length;
+        longestWord = word;
+    }
+}
+
+Console.WriteLine("The longest word in the provided string is...");
+Console.ReadLine();
+Console.WriteLine(longestWord);
