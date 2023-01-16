26156ef2237524509a9237e3b0011c1c09e684a1
brendantyler <galmanbrendan@gmail.com>
2023-01-15 22:18:11 -0600

    Add Part One

--- brendan-galman- ads-lab-1/Program.cs
+++ brendan-galman- ads-lab-1/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System.Globalization;
+using System.Linq;
+
+string sentenceInput = Console.ReadLine();
+string sentence = String.Join(" ", sentenceInput);
+char[] multipleOccurance = new char[] { };
+
+foreach (char letter in sentence)
+{
+    int count = 0;
+    for (int i = 0; i < sentence.Length; i++)
+    {
+        if (letter == sentence[i])
+        {
+            count++;
+        }
+    }
+    if (count > 1)
+    {
+        if (multipleOccurance.Contains(letter) == false)
+        {
+
+            Array.Resize(ref multipleOccurance, multipleOccurance.Length + 1);
+            multipleOccurance[multipleOccurance.Length - 1] = letter;
+        }
+
+    }
+}
+
+Console.Write("The Letter(s) That Appear Multiple Times are:");
+foreach (char letter in multipleOccurance)
+{
+    Console.Write($" {letter}");
+}
+
+Console.ReadLine();
