7dd048c3ccdc70648e9b814920f61047a776101c
brendantyler <galmanbrendan@gmail.com>
2023-01-18 04:02:02 -0600

    Add part 1

--- brendan-galman- ads-lab-1/Program.cs
+++ brendan-galman- ads-lab-1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int[] numArray = { 1, 2, 3, 4, 2, 9, 2, 4, 7};
+Array.Sort(numArray);
+
+//should return [2, 4]
+
+foreach (int num in numArray)
+{
+    int i = num - 1;
+
+    if (numArray[i] == 0)
+    {
+        numArray[i] = num;
+        Console.WriteLine(num);
+    }
+    else
+    {
+        numArray[i] = num - num;
+    }
+
+}
