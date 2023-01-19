483ec6ab416eb62c6877001ee0851c9b592828f8
brendantyler <galmanbrendan@gmail.com>
2023-01-19 01:41:30 -0600

    Add concat of array

--- MergeString/Program.cs
+++ MergeString/Program.cs
@@ -1,8 +1,29 @@
 ﻿int[] arr1 = { 1, 2, 3, 4, 5 };
 int[] arr2 = { 2, 5, 7, 9, 13 };
-
 // Program should return { 1, 2, 2, 3, 4, 5, 5, 7, 9, 13}
 
 // Combine strings 
+int[] arrFinal = arr1.Concat(arr2).ToArray();
 
-// Compare Values and sort
+Console.WriteLine(string.Join( " ", arrFinal));
+
+
+int holdNumber = 0;
+
+// Compare 
+for (int i = 0; i < arrFinal.Length; i++)
+{
+    for (int j = i + 1; j < arrFinal.Length; j++)
+    {   
+        if (arrFinal[i] > arrFinal[j])
+        {
+            holdNumber = arrFinal[i];
+            arrFinal[j] = arrFinal[i];
+            arrFinal[i] = holdNumber;
+            
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", arrFinal));
+
